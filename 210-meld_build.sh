f0e986bcde715eb92d9c65293b3bb9683c452a7d
René de Hesselle <dehesselle@web.de>
2024-07-16 15:03:10 +0200

    Add support for Meld CI

--- 210-meld_build.sh
+++ 210-meld_build.sh
@@ -36,9 +36,22 @@ bash_d_include error
 
 error_trace_enable
 
-#------------------------------------------------------------------ install Meld
+#-------------------------------------------------------------------- build Meld
 
-jhb build meld
+if [ "$CI_PROJECT_NAME" = "meld" ]; then
+  patch -p 1 < "$ETC_DIR/modulesets/meld/patches/meld-app.patch"
+
+  ln -s "$CI_PROJECT_DIR" "$SRC_DIR"/meld
+
+  jhb run meson setup \
+    --prefix "$VER_DIR" \
+    "$BLD_DIR/meld" \
+    "$CI_PROJECT_DIR"
+  jhb run meson compile -C "$BLD_DIR/meld"
+  jhb run meson install -C "$BLD_DIR/meld"
+else
+  jhb build meld
+fi
 
 # add build number to __version__ to AboutDialog and CLI help
 gsed -i "s/__version__/__version__ + ' ($MELD_BUILD)'/g" \
